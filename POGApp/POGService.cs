1e223b7db2bc3b2454497da8305f0b4b19214eb7
wouterthys <thys_wouter@hotmail.com>
2019-08-25 22:18:57 +0200

    Add project files.

---
+++ POGApp/POGService.cs
@@ -0,0 +1,163 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POGApp
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    public class POGService : IPOGService
+    {
+        private readonly Dictionary<Client, IPOGCallback> clients = new Dictionary<Client, IPOGCallback>();
+        private readonly List<Client> clientList = new List<Client>();
+
+        private readonly object syncObj = new object();
+
+        public IPOGCallback CurrentCallback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<IPOGCallback>();
+            }
+        }
+
+        private bool HasClient(string name)
+        {
+            foreach (Client c in clients.Keys)
+            {
+                if (c.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Connect(Client client)
+        {
+            if (!clients.ContainsValue(CurrentCallback) && !HasClient(client.Name))
+            {
+                lock (syncObj)
+                {
+                    clients.Add(client, CurrentCallback);
+                    clientList.Add(client);
+
+                    foreach (Client key in clients.Keys)
+                    {
+                        IPOGCallback callback = clients[key];
+                        try
+                        {
+                            callback.RefreshClients(clientList);
+                            callback.UserJoin(client);
+                        }
+                        catch
+                        {
+                            clients.Remove(key);
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public void Say(Message msg)
+        {
+            lock (syncObj)
+            {
+                foreach (IPOGCallback callback in clients.Values)
+                {
+                    callback.Receive(msg);
+                }
+            }
+        }
+
+        public void Whisper(Message msg, Client receiver)
+        {
+            foreach (Client rec in clients.Keys)
+            {
+                if (rec.Name == receiver.Name)
+                {
+                    IPOGCallback callback = clients[rec];
+                    callback.ReceiveWhisper(msg, rec);
+
+                    foreach (Client sender in clients.Keys)
+                    {
+                        if (sender.Name == msg.Sender)
+                        {
+                            IPOGCallback senderCallback = clients[sender];
+                            senderCallback.ReceiveWhisper(msg, rec);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void IsWriting(Client client)
+        {
+            lock (syncObj)
+            {
+                foreach (IPOGCallback callback in clients.Values)
+                {
+                    callback.IsWritingCallback(client);
+                }
+            }
+        }
+
+        public bool SendFile(FileMessage fileMsg, Client receiver)
+        {
+            foreach (Client rcvr in clients.Keys)
+            {
+                if (rcvr.Name == receiver.Name)
+                {
+                    Message msg = new Message
+                    {
+                        Sender = fileMsg.Sender,
+                        Content = "I'M SENDING FILE.. " + fileMsg.FileName
+                    };
+
+                    IPOGCallback rcvrCallback = clients[rcvr];
+                    rcvrCallback.ReceiveWhisper(msg, receiver);
+                    rcvrCallback.ReceiverFile(fileMsg, receiver);
+
+                    foreach (Client sender in clients.Keys)
+                    {
+                        if (sender.Name == fileMsg.Sender)
+                        {
+                            IPOGCallback sndrCallback = clients[sender];
+                            sndrCallback.ReceiveWhisper(msg, receiver);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Disconnect(Client client)
+        {
+            foreach (Client c in clients.Keys)
+            {
+                if (client.Name == c.Name)
+                {
+                    lock (syncObj)
+                    {
+                        clients.Remove(c);
+                        clientList.Remove(c);
+                        foreach (IPOGCallback callback in clients.Values)
+                        {
+                            callback.RefreshClients(clientList);
+                            callback.UserLeave(client);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ POGApp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POGApp
+{
+    class Program
+    {
+        private const string IP = "localhost";
+        private const int PORT = 9001;
+
+        private static ServiceHost host;
+
+        static void Main(string[] args)
+        {
+            InitServiceHost();
+
+            Console.ReadLine();
+        }
+
+        private static void InitServiceHost()
+        {
+            Uri tcpAdrs = new Uri("net.tcp://" +
+                IP + ":" +
+                PORT + "/Host/");
+
+            Uri httpAdrs = new Uri("http://" +
+                IP + ":" +
+                (PORT + 1).ToString() +
+                "/Host/");
+
+            Uri[] baseAdresses = { tcpAdrs, httpAdrs };
+
+            host = new ServiceHost(typeof(POGApp.POGService), baseAdresses);
+
+            NetTcpBinding tcpBinding = new NetTcpBinding(SecurityMode.None, true)
+            {
+                MaxBufferPoolSize = 67108864,
+                MaxBufferSize = 67108864,
+                MaxReceivedMessageSize = 67108864,
+                TransferMode = TransferMode.Buffered
+            };
+            tcpBinding.ReaderQuotas.MaxArrayLength = 67108864;
+            tcpBinding.ReaderQuotas.MaxBytesPerRead = 67108864;
+            tcpBinding.ReaderQuotas.MaxStringContentLength = 67108864;
+
+
+            tcpBinding.MaxConnections = 100;
+            //To maxmize MaxConnections you have 
+            //to assign another port for mex endpoint
+
+            //and configure ServiceThrottling as well
+            ServiceThrottlingBehavior throttle = host.Description.Behaviors.Find<ServiceThrottlingBehavior>();
+            if (throttle == null)
+            {
+                throttle = new ServiceThrottlingBehavior
+                {
+                    MaxConcurrentCalls = 100,
+                    MaxConcurrentSessions = 100
+                };
+                host.Description.Behaviors.Add(throttle);
+            }
+
+
+            //Enable reliable session and keep 
+            //the connection alive for 20 hours.
+            tcpBinding.ReceiveTimeout = new TimeSpan(20, 0, 0);
+            tcpBinding.ReliableSession.Enabled = true;
+            tcpBinding.ReliableSession.InactivityTimeout = new TimeSpan(20, 0, 10);
+
+            host.AddServiceEndpoint(typeof(POGApp.IPOGService), tcpBinding, "tcp");
+
+            //Define Metadata endPoint, So we can 
+            //publish information about the service
+            ServiceMetadataBehavior mBehave = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(mBehave);
+
+            host.AddServiceEndpoint(typeof(IMetadataExchange),
+                MetadataExchangeBindings.CreateMexTcpBinding(),
+                "net.tcp://" + IP + ":" +
+                (PORT - 1).ToString() +
+                "/Host/mex");
+
+
+            try
+            {
+                host.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error opening host: " + ex);
+            }
+            finally
+            {
+                if (host.State == CommunicationState.Opened)
+                {
+                    Console.WriteLine("Opened");
+                    //labelStatus.Content = "Opened";
+                    //buttonStop.IsEnabled = true;
+                }
+            }
+        }
+    }
+}
